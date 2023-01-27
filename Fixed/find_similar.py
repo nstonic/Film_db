d3ba35c8e3f7f7cb4b8056715f52c3e8aaf3c732
nstonic <118763806+nstonic@users.noreply.github.com>
2023-01-27 21:41:15 +0400

    Refactor Fixed\find_similar.py

--- Fixed/find_similar.py
+++ Fixed/find_similar.py
@@ -15,13 +15,13 @@ def calculate_points(user_film: dict, comparing_film: dict) -> int:
                if comparing_film[param] == user_film[param])
 
 
-def get_recommendations(user_film: dict, films: dict[dict]) -> list[str]:
+def get_recommendations(user_film: dict, films: dict[dict]) -> list[tuple[str, int]]:
     ratings = {
         comparing_film["original_title"]: calculate_points(user_film, comparing_film)
         for comparing_film in films
         if comparing_film != user_film
     }
-    
+
     return sorted(
         ratings.items(),
         key=lambda rating: rating[1],
@@ -31,18 +31,14 @@ def get_recommendations(user_film: dict, films: dict[dict]) -> list[str]:
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument(
-        "--FilmsDB",
-        dest="db_file",
-        default="MyFilmDB.json",
-        help="Путь к файлу с базой фильмов. По умолчанию MyFilmDB.json"
-    )
-    parser.add_argument(
-        "--top",
-        type=int,
-        default=8,
-        help="Сколько рекомендаций вывести. Поумолчанию 8"
-    )
+    parser.add_argument("--FilmsDB",
+                        dest="db_file",
+                        default="MyFilmDB.json",
+                        help="Путь к файлу с базой фильмов. По умолчанию MyFilmDB.json")
+    parser.add_argument("--top",
+                        type=int,
+                        default=8,
+                        help="Сколько рекомендаций вывести. Поумолчанию 8")
     args = parser.parse_args()
 
     with open(args.db_file, encoding='utf-8') as file:
@@ -54,11 +50,11 @@ def main():
     except StopIteration:
         print('No such film in FilmsDB')
         return
-        
+
     recommendations = get_recommendations(user_film, films)
     max_points = 1900
     for index, recommendation in enumerate(recommendations[:args.top]):
-        print(f"{index+1}) {recommendation[0]} - matching {recommendation[1]*100/max_points}%")
+        print(f"{index + 1}) {recommendation[0]} - matching {recommendation[1] / max_points:.0%}")
 
 
 if __name__ == '__main__':
